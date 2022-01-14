7de77af1b0578c59f7f1bc07c8e5e7bed3a857b6
janzheng <janeazy@gmail.com>
2022-01-13 20:16:09 -0500

    oops

--- node_modules/calendar-link/dist/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const TimeFormats: {
-    dateTimeWithOffset: string;
-    dateTimeUTC: string;
-    allDay: string;
-};
--- node_modules/calendar-link/dist/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TimeFormats = void 0;
-exports.TimeFormats = {
-    dateTimeWithOffset: "YYYY-MM-DD[T]HH:mm:ssZ",
-    dateTimeUTC: "YYYYMMDD[T]HHmmss[Z]",
-    allDay: "YYYYMMDD",
-};
-//# sourceMappingURL=utils.js.map
