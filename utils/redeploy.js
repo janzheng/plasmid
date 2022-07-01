742a19a315ea9141f323e2d590a4774599c9e384
janzheng <janeazy@gmail.com>
2022-07-01 10:38:27 +1000

    update

--- utils/redeploy.js
+++ utils/redeploy.js
@@ -6,21 +6,43 @@
 
 export const redeploy = ({ url }) => {
   let secret = url.searchParams.get('secret')
-  if (import.meta.env.VITE_DEPLOY_SECRET && secret == import.meta.env.VITE_DEPLOY_SECRET) {
+  if (process.env.VITE_DEPLOY_SECRET && secret == process.env.VITE_DEPLOY_SECRET) {
     return {
       status: 302,
-      headers: { Location: import.meta.env.VITE_DEPLOY }
+      headers: { Location: process.env.VITE_DEPLOY }
     }
   }
-  else if (!import.meta.env.VITE_DEPLOY_SECRET) {
+  else if (!process.env.VITE_DEPLOY_SECRET) {
     return {
       status: 302,
-      headers: { Location: import.meta.env.VITE_DEPLOY }
+      headers: { Location: process.env.VITE_DEPLOY }
     }
   }
 
   // bad request // secret didn't work
   return { status: 500, body: 'incorrect secret' }
 }
+
+
+
+// export const redeploy = ({ url }) => {
+//   let secret = url.searchParams.get('secret')
+//   console.log('vite deploy secret: ', import.meta.env.VITE_DEPLOY_SECRET)
+//   if (import.meta.env.VITE_DEPLOY_SECRET && secret == import.meta.env.VITE_DEPLOY_SECRET) {
+//     return {
+//       status: 302,
+//       headers: { Location: import.meta.env.VITE_DEPLOY }
+//     }
+//   }
+//   else if (!import.meta.env.VITE_DEPLOY_SECRET) {
+//     return {
+//       status: 302,
+//       headers: { Location: import.meta.env.VITE_DEPLOY }
+//     }
+//   }
+
+//   // bad request // secret didn't work
+//   return { status: 500, body: 'incorrect secret' }
+// }
 
 
