3b2a9e4b011ae1418eb1c514caa5fc6c5498e20e
janzheng <janeazy@gmail.com>
2022-09-29 06:35:00 +1000

    localized calendar-link

--- utils/events/event.js
+++ utils/events/event.js
@@ -8,9 +8,10 @@
 import { cytosis, getContent } from "$plasmid/utils/airfetch.js"
 
 // import send from '@polka/send';
+// const { google, outlook, office365, yahoo, ics, eventify } = require("calendar-link");
 // import { google, outlook, office365, yahoo, ics } from "calendar-link";
-import * as cal from 'calendar-link'; // eventify not getting imported
-const { google, outlook, office365, yahoo, ics, eventify } = cal;
+// import * as cal from 'calendar-link'; // eventify not getting imported
+// const { google, outlook, office365, yahoo, ics, eventify } = cal;
 
 
 import { config } from "dotenv";
@@ -121,8 +122,8 @@ export async function getEventLinks(event) {
   if (!event) await getEventFromContent()
   try {
     return {
-      googleLink: google(event),
-      yahooLink: yahoo(event),
+      google: googleLink(event),
+      yahoo: yahooLink(event),
       officeLink: office365(event),
       outlook: outlook(event),
       ics: ics(event),
@@ -142,10 +143,10 @@ export async function getEventLinks(event) {
 
 //     if (req.query.links) {
 //       sendData({
-//         googleLink: google(event),
-//         yahooLink: yahoo(event),
+//         googleLink(event),
+//         yahooLink(event),
 //         officeLink: office365(event),
-//         outlook: outlook(event),
+//         outlook(event),
 //       }, res);
 //     } else {
 //       send(res, 200, decodeURIComponent(customIcs(event).split("charset=utf8,")[1]), {
@@ -278,6 +279,10 @@ export const customIcs = (calendarEvent) => {
 
 
 
+
+// embedding calendar-link here b/c of UMD/ES6 issues
+
+
 const TimeFormats = {
   dateTimeWithOffset: "YYYY-MM-DD[T]HH:mm:ssZ",
   dateTimeUTC: "YYYYMMDD[T]HHmmss[Z]",
@@ -287,3 +292,183 @@ function formatTimes({ allDay, startUtc, endUtc, alarmUtc }, dateTimeFormat) {
   const format = TimeFormats[allDay ? "allDay" : dateTimeFormat];
   return { start: startUtc.format(format), end: endUtc.format(format) };
 }
+
+
+
+
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import { stringify } from "query-string";
+
+dayjs.extend(utc);
+
+export const eventify = (event) => {
+  const { start, end, duration, ...rest } = event;
+  const startUtc = dayjs(start).utc();
+  const endUtc = end
+    ? dayjs(end).utc()
+    : (() => {
+      if (event.allDay) {
+        return startUtc.add(1, "day");
+      }
+      if (duration && duration.length == 2) {
+        const value = Number(duration[0]);
+        const unit = duration[1];
+        return startUtc.add(value, unit);
+      }
+      return dayjs().utc();
+    })();
+  return {
+    ...rest,
+    startUtc,
+    endUtc,
+  };
+};
+
+export const google = (calendarEvent) => {
+  const event = eventify(calendarEvent);
+  const { start, end } = formatTimes(event, event.allDay ? "allDay" : "dateTimeUTC");
+  const details = {
+    action: "TEMPLATE",
+    text: event.title,
+    details: event.description,
+    location: event.location,
+    trp: event.busy,
+    dates: start + "/" + end,
+  };
+  if (event.guests && event.guests.length) {
+    details.add = event.guests.join();
+  }
+  return `https://calendar.google.com/calendar/render?${stringify(details)}`;
+};
+
+export const outlook = (calendarEvent) => {
+  const event = eventify(calendarEvent);
+  const { start, end } = formatTimes(event, "dateTimeWithOffset");
+  const details = {
+    path: "/calendar/action/compose",
+    rru: "addevent",
+    startdt: start,
+    enddt: end,
+    subject: event.title,
+    body: event.description,
+    location: event.location,
+    allday: event.allDay || false
+  };
+  return `https://outlook.live.com/calendar/0/deeplink/compose?${stringify(details)}`;
+};
+
+export const office365 = (calendarEvent) => {
+  const event = eventify(calendarEvent);
+  const { start, end } = formatTimes(event, "dateTimeWithOffset");
+  const details = {
+    path: "/calendar/action/compose",
+    rru: "addevent",
+    startdt: start,
+    enddt: end,
+    subject: event.title,
+    body: event.description,
+    location: event.location,
+    allday: event.allDay || false
+  };
+  return `https://outlook.office.com/calendar/0/deeplink/compose?${stringify(details)}`;
+};
+
+export const yahoo = (calendarEvent) => {
+  const event = eventify(calendarEvent);
+  const { start, end } = formatTimes(event, event.allDay ? "allDay" : "dateTimeUTC");
+  const details = {
+    v: 60,
+    title: event.title,
+    st: start,
+    et: end,
+    desc: event.description,
+    in_loc: event.location,
+    dur: event.allDay ? "allday" : false
+  };
+  return `https://calendar.yahoo.com/?${stringify(details)}`;
+};
+
+export const ics = (calendarEvent) => {
+  const event = eventify(calendarEvent);
+  const formattedDescription = (event.description || "")
+    .replace(/,/gm, ",")
+    .replace(/;/gm, ";")
+    .replace(/\r\n/gm, "\n")
+    .replace(/\n/gm, "\\n")
+    .replace(/(\\n)[\s\t]+/gm, "\\n");
+
+  const formattedLocation = (event.location || "")
+    .replace(/,/gm, ",")
+    .replace(/;/gm, ";")
+    .replace(/\r\n/gm, "\n")
+    .replace(/\n/gm, "\\n")
+    .replace(/(\\n)[\s\t]+/gm, "\\n");
+
+  const { start, end } = formatTimes(event, event.allDay ? "allDay" : "dateTimeUTC");
+  const calendarChunks = [
+    {
+      key: "BEGIN",
+      value: "VCALENDAR",
+    },
+    {
+      key: "VERSION",
+      value: "2.0",
+    },
+    {
+      key: "BEGIN",
+      value: "VEVENT",
+    },
+    {
+      key: "URL",
+      value: event.url,
+    },
+    {
+      key: "DTSTART",
+      value: start,
+    },
+    {
+      key: "DTEND",
+      value: end,
+    },
+    {
+      key: "SUMMARY",
+      value: event.title,
+    },
+    {
+      key: "DESCRIPTION",
+      value: formattedDescription,
+    },
+    {
+      key: "LOCATION",
+      value: formattedLocation,
+    },
+    {
+      key: "ORGANIZER",
+      value: event.organizer,
+    },
+    {
+      key: "END",
+      value: "VEVENT",
+    },
+    {
+      key: "END",
+      value: "VCALENDAR",
+    },
+  ];
+
+  let calendarUrl = "";
+
+  calendarChunks.forEach((chunk) => {
+    if (chunk.value) {
+      if (chunk.key == "ORGANIZER") {
+        const value = chunk.value;
+        calendarUrl += `${chunk.key};${encodeURIComponent(`CN=${value.name}:MAILTO:${value.email}\n`)}`;
+      } else {
+        calendarUrl += `${chunk.key}:${encodeURIComponent(`${chunk.value}\n`)}`;
+      }
+    }
+  });
+
+  return `data:text/calendar;charset=utf8,${calendarUrl}`;
+};
