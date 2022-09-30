4f05763ac4df9f4e5e68b1bd4987c4e047b0d424
janzheng <janeazy@gmail.com>
2022-09-30 12:11:10 +1000

    encodes urls

--- utils/scrapingbee.js
+++ utils/scrapingbee.js
@@ -14,7 +14,11 @@ export const getBee = async ({
     result = ""
     let beeresults = ``
     let __fetch = _fetch || fetch // for server-side or client-side
-    const response = await __fetch(`https://app.scrapingbee.com/api/v1?api_key=${api_key}&url=${url}&render_js=${render_js}&block_resources=${block_resources}&timeout=${timeout}&extract_rules=${extract_rules}`)
+
+    const str = `https://app.scrapingbee.com/api/v1?api_key=${api_key}&url=${encodeURIComponent(url)}&render_js=${render_js}&block_resources=${block_resources}&timeout=${timeout}&extract_rules=${extract_rules}`
+    console.log('[scrapingbee] fetching', str)
+    const response = await __fetch(str)
+    
 
     // success
     if (response.status == 200) {
@@ -22,9 +26,10 @@ export const getBee = async ({
       console.log('bee init results:', beeresults)
       result = beeresults
       console.log('bee text:', result)
+      return result
     }
 
-    return result
+    console.log(response)
   } catch (err) {
     console.error('scrapingbee error:', err)
     isScraping = false
