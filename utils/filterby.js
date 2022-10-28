99d413d1b4db25e5624a24286d78ed454ff68a3a
Jan Zheng <janzheng@Jans-CrazyBook-Pro.local>
2022-10-28 14:43:09 +1100

    helpers

--- utils/filterby.js
+++ utils/filterby.js
@@ -2,6 +2,13 @@
 // 
 // convenience functions
 // 
+
+// 
+// $space = filterBy(orgSettings.spaces, 'name', data.space)?.[0]
 export const filterBy = (arr, key, value) => {
   return arr.filter((item) => item[key] === value);
+};
+
+export const filterFind = (arr, key, value) => {
+  return arr.filter((item) => item[key] === value)?.[0] || null;
 };
