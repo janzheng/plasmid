b8b32e218cd0bc74c07314d1139f18e0b5e25740
Jan Zheng <janzheng@Jans-CrazyBook-Pro.local>
2023-03-10 10:51:26 +1100

    update

--- utils/markdown/rehype.js
+++ utils/markdown/rehype.js
@@ -15,7 +15,7 @@ import rehypeStringify from 'rehype-stringify'
 // uses rehype to convert HTML that contains raw markdown to markdown, then uses remark to convert markdown to HTML. Useful for converting Notion's HTML to markdown when it includes citations / footnotes
 export const htmlToMdToHtml = async (html = '# Hello, Neptune!') => {
 
-  console.log('htmlToMdToHtml:::', html)
+  // console.log('htmlToMdToHtml:::', html)
   let md = await unified()
     .use(rehypeParse)
     // .use(remarkToc, { heading: 'contents' })
@@ -31,7 +31,7 @@ export const htmlToMdToHtml = async (html = '# Hello, Neptune!') => {
     md = String(md).replaceAll('\\~', '\~') 
     // md = String(md).replaceAll('\\*', '\\*') // this keeps escaping author note, but migh break bullets?
 
-    console.log('md:::', md?.value || md)
+    // console.log('md:::', md?.value || md)
 
   let _html = String(await unified()
     .use(remarkParse)
