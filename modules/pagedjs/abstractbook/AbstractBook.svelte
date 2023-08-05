d4c6cdb87543714b1270647297ccfdfc288980c1
Jan Zheng <janzheng@Jans-CrazyBook-Pro.local>
2023-08-05 11:44:29 -0700

    absbook bump

--- modules/pagedjs/abstractbook/AbstractBook.svelte
+++ modules/pagedjs/abstractbook/AbstractBook.svelte
@@ -33,80 +33,188 @@
     </div>
   </div>
   
-  <section class="tableofcontents pagebreak reset-toc-page text-xs" id="toc" >
-    {#each sessions as session}
-      <div class="session-container mb-2">
-        <div class="text-base font-serif">{session}</div>
-        <ul>
-          {#each abstracts.filter(abs => abs['Assignment'] == session) as abstract}
-            <a id="toc-{abstract['AbstractId']}" href="#abstract-{abstract['AbstractId']}" 
-              class="Abstract-session-toc | underline-none text-black relative ">
-              <li class="list-none mb-0 pr-8 pfix">
-                <span>{@html abstract['TitlePlain']}</span>
-              </li>
-            </a>
-          {/each}
-        </ul>
+  <!-- title page front [color] -->
+
+  <!-- title page back [color] -->
+  <div class="Sponsor TAILOR one frontpage pagebreak">
+    <!-- <h2 class="pt-0 pb-0">Sponsor page | TAILOR 1 Full [color]</h2> -->
+    <div class="ad">
+      <!-- <h2 class="pt-0 pb-0">TAILOR 1 Full [color]</h2> -->
+      <img src="/ad-placeholder.png" width="100%" height="705px" alt="Ad Placement"/>
+    </div>
+  </div>
+
+  <div class="Sponsor temp content holder pagebreak">
+    <h2 class="pt-0 pb-0">Temp Content Holder Middle pages // content</h2>
+  </div>
+
+  <!-- sponsor page front [color] -->
+  <div class="Sponsor TAILOR two backpage pagebreak">
+    <!-- <h2 class="pt-0 pb-0">TAILOR 2 Full [color]</h2> -->
+    <img src="/ad-placeholder.png" width="100%" height="705px" alt="Ad Placement"/>
+  </div>
+
+  <div class="Sponsor JAFRAL pagebreak">
+    <!-- <h2 class="pt-0 pb-0">JAFRAL Full [color]</h2> -->
+    <img src="/ad-placeholder.png" width="100%" height="705px" alt="Ad Placement"/>
+  </div>
+
+  <div class="Sponsor PHASEGenomics pagebreak">
+    <!-- <h2 class="pt-0 pb-0">PHASEGenomics Full [color]</h2> -->
+    <img src="/ad-placeholder.png" width="100%" height="705px" alt="Ad Placement"/>
+  </div>
+
+  <div class="Sponsor MAL PHAGE halfpage pagebreak">
+    <!-- <h2 class="pt-0 pb-0">MALPHAGE Genomics Full [color]</h2> -->
+    <img src="/ad-placeholder.png" width="100%" height="705px" alt="Ad Placement"/>
+  </div>
+
+
+  <!-- back page front [color] - logo grid -->
+  <div class="Backpage front pagebreak">
+    <h2 class="pt-0 pb-0">Many thanks to our sponsors</h2>
+    <div class="block grid grid-cols-2 gap-0">
+      <div class=" p-2 text-center | mb-2 mb-0 | flex justify-center items-center">
+        <a href="https://www.bcm.edu/research/research-centers/tailor" >
+          <img class="inline" src="https://f2.phage.directory/airscripts/-evergreen-live/content/recbkFru4OHILuL7p/tailor-logo-sm1.jpg" alt="TAILOR Labs" />
+        </a>
       </div>
-    {/each}
-  </section>   
 
-  
-  <div class="Session-Abstracts">
-    {#each sessions as session}
-      <div class="Session">
-        <div class="session-title pagebreak font-serif text-4xl mt-80 w-[505px] text-center px-8">{session}</div>
-        <div class="">
-          {#each abstracts.filter(abs => abs['Assignment'] == session) as abstract}
-            {@const authorAffiliations = getAuthorAffiliations(abstract)}
-            {@const textLen = abstract['Body'].length + JSON.stringify(authorAffiliations).length}
-            <div id={'abstract-'+abstract['AbstractId']}></div>
-            <div class="Abstract page-container pagebreak w-full bg-blue-200 text-xs">
-              <div class="header text-xxs">
-                {session} — Abstract ID: {abstract.AbstractId}
-              </div>
-              <hr class="pb-1"/>
-              {#if abstract?.Keywords}
-                <div class="tags">
-                  {#each abstract?.Keywords?.split(",") as tag}
-                    <span class="Btn --tag --snug --thin text-xxs text-gray-900 bg-gray-100 | mr-1 mb-1-i">{tag}</span>
-                  {/each}
-                </div>
-              {/if}
-              <div class="font-serif text-base leading-tight w-full pfix mb-1">{@html marked(abstract['TitlePlain']||'')}</div>
-              <div class="Abstract-textbody
-                {textLen <= 2600 ? 'text-xs' : ''}
-                {textLen > 2600 && textLen <= 3900 ? 'text-xxs' : ''}
-                {textLen > 3900 ? 'text-[9px] leading-[11px]' : ''}
-              ">
-                {#if authorAffiliations}
-                  <div class="Author-Affiliations">
-                    <div class="pfix mb-[0.2rem]">{@html marked(authorAffiliations?.authors.join(", ") || '')}</div>
-                    <ol class="">
-                      {#each authorAffiliations?.affiliations as aff}
-                        <li class="">{aff}</li>
-                      {/each}
-                    </ol>
-                  </div>
-                {:else}
-                  <div class="pfix">{@html marked(abstract.authors || '')}</div>
-                  <div class="whitespace-pre-line affiliations pfix">{@html marked(abstract.affiliations || '')}</div>
-                {/if}
-
-                {#if abstract.correspondence}
-                  <div class="pfix">{@html marked(abstract.correspondence || '')}</div>
-                {/if}
-
-                <div class="mt-2">
-                  {@html marked(abstract['Body']||'')}
-                </div>
-              </div>
-            </div>
-          {/each}
+      <div class=" p-2 text-center | mb-2 mb-0 | flex flex-col justify-center items-center">
+        <a href="https://jafral.com" >
+          <img class="inline" src="https://evergreen.phage.directory/JAFRAL.png" alt="JAFRAL" />
+        </a>
+        <div class="text-xs pt-2">THE phage contract manufacturer for GMP and non-GMP products
         </div>
       </div>
-    {/each}
+
+      <div class=" p-2 text-center | mb-2 mb-0 | flex flex-col justify-center items-center">
+        <a href="https://phasegenomics.com" >
+          <img class="inline" src="https://phasegenomics.com/wp-content/uploads/2016/12/phase-genomics-nav-logo.png" alt="Phase Genomics" />
+        </a>
+        <div class="text-xs pt-2">Phase Genomics applies ultra-long-range sequencing methods to enable metagenomic discovery, chromosome-scale genome assembly, and more
+        </div>
+      </div>
+
+      <div class=" p-2 text-center | mb-2 mb-0 |">
+        <div class="flex justify-center items-center">
+        <a href="https://www.liebertpub.com/phage" >
+        <img class="inline" style="width: 100%;" src="https://evergreen.phage.directory/Phage-logo-outlines-color.svg" alt="PHAGE" />
+        </a>
+        </div>
+        <div class="text-xs pt-2">The only peer-reviewed journal dedicated to bacteriophage research and its applications in medicine, agriculture, aquaculture, veterinary applications, animal production, food safety, and food production.</div>
+      </div>
+
+      <div class=" p-2 text-center mb-2 mb-0  align-middle | flex justify-center items-center">
+        <a href="https://www.phagesforglobalhealth.org/"><img class="inline" style="width: 100%; max-height: 120px" src="https://evergreen.phage.directory/pgh_logo_400.jpeg" alt="Phages for Global Health" /></a>
+      </div>
+
+      <div class=" p-2 text-center mb-2 mb-0  align-middle | flex justify-center items-center">
+        <a href="https://rime-bioinformatics.com/"><img class="inline" style="width: 100%; max-height: 120px" src="https://f2.phage.directory/airscripts/-evergreen-2023-live/content/reckuB3EZqMu83Z3a/rime_color_sm.png" alt="Phages for Global Health" /></a>
+      </div>
+
+      <div class=" p-2 text-center mb-2 mb-0  align-middle | flex justify-center items-center">
+        <a href="https://www.mdpi.com/journal/ddc?ci=33011&si=26552&_utm_from=53f6064e67"><img class="inline" style="width: 100%; max-height: 120px" src="https://f2.phage.directory/airscripts/-evergreen-2023-live/content/reckuB3EZqMu83Z3a/Drugs-and-Drug-Candidates-partnership.png" alt="MDPI Drugs and Drug Candidates" /></a>
+      </div>
+
+      <div class=" p-2 text-center mb-2 mb-0  align-middle | flex justify-center items-center">
+        <a href="https://phage.health"><img class="inline" style="width: 100%; max-height: 120px" src="https://phage.health/wp-content/uploads/2021/09/logo-default.png" alt="Vesale Bioscience" /></a>
+      </div>
+
+
+      <div class=" p-2 text-center mb-2 mb-0 
+      align-middle | flex justify-center items-center">
+        <a href="https://phage.directory/"><img class="inline" style="width: 100%;" src="https://dl.phage.directory/MpmrA9ETYuIp88eMbpao_full_phagedirectory2x.png" alt="Phage Directory" /></a>
+      </div>
+
+      <div class=" p-2 text-center mb-2 mb-0 
+      align-middle | flex justify-center items-center">
+        <a class="text-evg-green-dark font-bold text-xl font-serif" href="https://phagefound.org">Phagebiotics Research Foundation</a>
+      </div>
+
+    </div>
   </div>
+
+  <!-- back page back [color] -->
+  <div class="Backpage back pagebreak">
+    <h2 class="pt-0 pb-0">Back page | Back [color]</h2>
+  </div>
+  
+  {#if renderSessions}
+    <section class="tableofcontents pagebreak reset-toc-page text-xs" id="toc" >
+      {#each sessions as session}
+        <div class="session-container mb-2">
+          <div class="text-base font-serif">{session}</div>
+          <ul>
+            {#each abstracts.filter(abs => abs['Assignment'] == session) as abstract}
+              <a id="toc-{abstract['AbstractId']}" href="#abstract-{abstract['AbstractId']}" 
+                class="Abstract-session-toc | underline-none text-black relative ">
+                <li class="list-none mb-0 pr-8 pfix">
+                  <span>{@html abstract['TitlePlain']}</span>
+                </li>
+              </a>
+            {/each}
+          </ul>
+        </div>
+      {/each}
+    </section>   
+
+    <div class="Session-Abstracts">
+      {#each sessions as session}
+        <div class="Session">
+          <div class="session-title pagebreak font-serif text-4xl mt-80 w-[505px] text-center px-8">{session}</div>
+          <div class="">
+            {#each abstracts.filter(abs => abs['Assignment'] == session) as abstract}
+              {@const authorAffiliations = getAuthorAffiliations(abstract)}
+              {@const textLen = abstract['Body'].length + JSON.stringify(authorAffiliations||"").length}
+              <div id={'abstract-'+abstract['AbstractId']}></div>
+              <div class="Abstract page-container pagebreak w-full bg-blue-200 text-xs">
+                <div class="header text-xxs">
+                  {session} — Abstract ID: {abstract.AbstractId}
+                </div>
+                <hr class="pb-1"/>
+                {#if abstract?.Keywords}
+                  <div class="tags">
+                    {#each abstract?.Keywords?.split(",") as tag}
+                      <span class="Btn --tag --snug --thin text-xxs text-gray-900 bg-gray-100 | mr-1 mb-1-i">{tag}</span>
+                    {/each}
+                  </div>
+                {/if}
+                <div class="font-serif text-base leading-tight w-full pfix mb-1">{@html marked(abstract['TitlePlain']||'')}</div>
+                <div class="Abstract-textbody
+                  {textLen <= 2600 ? 'text-xs' : ''}
+                  {textLen > 2600 && textLen <= 3900 ? 'text-xxs' : ''}
+                  {textLen > 3900 ? 'text-[9px] leading-[11px]' : ''}
+                ">
+                  {#if authorAffiliations}
+                    <div class="Author-Affiliations">
+                      <div class="pfix mb-[0.2rem]">{@html marked(authorAffiliations?.authors.join(", ") || '')}</div>
+                      <ol class="">
+                        {#each authorAffiliations?.affiliations || [] as aff}
+                          <li class="">{aff}</li>
+                        {/each}
+                      </ol>
+                    </div>
+                  {:else}
+                    <div class="pfix">{@html marked(abstract.authors || '')}</div>
+                    <div class="whitespace-pre-line affiliations pfix">{@html marked(abstract.affiliations || '')}</div>
+                  {/if}
+
+                  {#if abstract.correspondence}
+                    <div class="pfix">{@html marked(abstract.correspondence || '')}</div>
+                  {/if}
+
+                  <div class="mt-2">
+                    {@html marked(abstract['Body']||'')}
+                  </div>
+                </div>
+              </div>
+            {/each}
+          </div>
+        </div>
+      {/each}
+    </div>
+  {/if}
 
   {#if renderAuthorIndex}
     <div id="author-index" class="author-index pagebreak text-xxs"> 
@@ -148,6 +256,7 @@
   })
 
   export let abstracts, sessions, authors;
+  export let renderSessions = false;
   export let renderAuthorIndex = false;
   // abstracts = abstracts.slice(0,10); // limit for dev
   console.log('author list:', authors)
