f3c5f9392c5c592ec53027db32d08963c4fedfad
Jan Zheng <janzheng@Jans-CrazyBook-Pro.local>
2023-10-29 12:34:27 +1100

    bump

--- modules/llm/fquery.js
+++ modules/llm/fquery.js
@@ -80,7 +80,7 @@ export const fQuery = (config) => {
   try {
     const OPENAI_API_KEY = config?.apiKey || process.env.OPENAI_API_KEY;
 
-    console.log('::::: config::', config, process.env)
+    // console.log('::::: config::', config, process.env)
     
     if (!OPENAI_API_KEY) {
       throw new Error('OPENAI_API_KEY env variable not set!')
